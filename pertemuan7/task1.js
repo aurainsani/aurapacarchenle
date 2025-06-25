ae605a6f72b36ca94d59a0136be8936c57d7d97f
aurainsani <aurainsani2419@gmail.com>
2025-06-25 17:31:14 +0700

    update

---
+++ pertemuan7/task1.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Tasya';
+let lastName = 'Amelia';
+let bornYear = 2006;
+
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
